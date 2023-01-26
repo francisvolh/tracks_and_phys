44d8cda07a69a7e402b4ad13f03ab344eb27a027
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-01-26 13:33:19 -0500

    refining plot

--- scripts/mapping_tracks_dives.R
+++ scripts/mapping_tracks_dives.R
@@ -15,7 +15,7 @@ nrow(depth_gps_data %>%
   filter(in_dive == 1) )
 
 depth_gps_data %>% 
-  group_by(species) %>% #or per dep ID?
+  group_by(dep_id) %>% #or per dep ID?
   filter(in_dive == 1) %>% 
   summarise(
     MaxDepth = max(depth),
@@ -23,14 +23,29 @@ depth_gps_data %>%
     meanDepth = mean(depth),
     sdDepth = sd(depth),
     .groups = "drop" 
-  )
+  ) %>% 
+  View()
 
 #need to add the base map! or a terrain basemap
+
+world <- sf::st_read("data/gadm36_PER_shp/gadm36_PER_0.shp")
+
 depth_gps_data %>% 
   filter(in_dive == 1) %>%
-  filter(species == "PEBO") %>% 
+  #filter(species == "PEBO") %>% 
   ggplot()+
+  geom_sf(data = world, aes()) +  ##add basemap
+  coord_sf(crs = 4326, xlim = range(depth_gps_data$lon), ylim = range(depth_gps_data$lat))+# +
   geom_point(
-    aes(x= lon, y=lat, colour = as.factor(in_dive)), cex = 0.01
-  )
+    aes(x= lon, y=lat, colour = as.factor(species)), cex = 0.01
+  )+
+  theme_bw()+
+  xlab("Longitude")+
+  ylab("Latitude")+
+  theme( legend.position = "none")
+
+#maybe plot the change in dive locations with a change in day, every two days or so
+
+# refine the depth? more than 0.7? or check the histograms for shallow dives?
+
 
