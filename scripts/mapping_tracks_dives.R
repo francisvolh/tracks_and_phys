fb2a35e98cdc6a581de624395445c8a85be231a6
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2023-01-27 01:33:12 -0500

    mapping dives points and kernels first trial

--- scripts/mapping_tracks_dives.R
+++ scripts/mapping_tracks_dives.R
@@ -32,7 +32,7 @@ world <- sf::st_read("data/gadm36_PER_shp/gadm36_PER_0.shp")
 
 depth_gps_data %>% 
   filter(in_dive == 1) %>%
-  #filter(species == "PEBO") %>% 
+  filter(species == "PEBO") %>% 
   ggplot()+
   geom_sf(data = world, aes()) +  ##add basemap
   coord_sf(crs = 4326, xlim = range(depth_gps_data$lon), ylim = range(depth_gps_data$lat))+# +
@@ -48,4 +48,122 @@ depth_gps_data %>%
 
 # refine the depth? more than 0.7? or check the histograms for shallow dives?
 
+#kernels
 
+for_kernel <-depth_gps_data %>% 
+  filter(in_dive == 1) %>% 
+  mutate(
+    colorKernel = case_when(
+      species == "PEBO" ~ "cyan",
+      species == "GUCO" ~ "brown1"
+    )
+  )
+
+for (i in unique(for_kernel$species)) {
+  
+  oneSpecies <- for_kernel %>% 
+    filter(species == i)
+  
+  onespf <- sf::st_as_sf(oneSpecies, 
+                         coords = c("lon","lat"),
+                         crs = sf::st_crs(4326))
+  
+  # convert to sp object if needed
+  one.sp.points <- as(onespf, "Spatial")
+  
+  onekernel<-adehabitatHR::kernelUD(one.sp.points)
+  
+  kernelTest50<-adehabitatHR::getverticeshr(onekernel, 50)
+  kernelTest75<-adehabitatHR::getverticeshr(onekernel, 75)
+  kernelTest95<-adehabitatHR::getverticeshr(onekernel, 95)
+  
+  
+  
+  p <-ggplot() + 
+    geom_sf(data= sf::st_as_sf(kernelTest50), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
+    )+
+    geom_sf(data= sf::st_as_sf(kernelTest75), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel) #, aes(fill = id, alpha = 0.5)
+    )+
+    geom_sf(data= sf::st_as_sf(kernelTest95), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
+    )+
+    #geom_sf(data= sf::st_as_sf(one.grizzly.mcp), aes(fill = id, alpha = 0.5)) +
+    #scale_fill_discrete(name = "Animal id")+
+    #geom_point(data = xy.obs, aes(x=LONGITUDE , y=LATITUDE))+
+    geom_sf(data = world) +
+    coord_sf(crs = 4326, xlim = range(for_kernel$lon), ylim = range(for_kernel$lat)) +
+    #annotate(geom="text", x=-78.6, y=-8.4, label="2018", size= 13,
+    #        color="black") +
+    guides(color = guide_legend(override.aes = list(size = 4)) )+ 
+    guides(alpha = "none", color = "none", size = "none")+
+    ggtitle(i)+
+    theme_bw()+
+    theme(legend.position = "none")
+  print(p)
+  
+}
+
+
+trialspdf <- sf::st_as_sf(for_kernel, 
+                       coords = c("lon","lat"),
+                       crs = sf::st_crs(4326))
+trialspdf <- as(trialspdf, "Spatial")
+
+trialkernel<-adehabitatHR::kernelUD(trialspdf[,8])
+
+
+image(trialkernel[[2]])
+
+plot(getverticeshr(trialkernel[[1]], 95), add = TRUE)
+
+# creating SpatialPolygonsDataFrame
+
+kd_names <- names(trialkernel)
+
+ud <- lapply(trialkernel, function(x) try(getverticeshr(x, 95)))
+
+# changing each polygons id to the species name for rbind call
+
+sapply(1:length(ud), function(i) {
+  row.names(ud[[i]]) <<- kd_names[i]
+})
+sdf_poly <- Reduce(rbind, ud)
+plot(sdf_poly)
+
+ud2 <- lapply(trialkernel, function(x) try(getverticeshr(x, 70)))
+
+# changing each polygons id to the species name for rbind call
+
+sapply(1:length(ud2), function(i) {
+  row.names(ud2[[i]]) <<- kd_names[i]
+})
+sdf_poly2 <- Reduce(rbind, ud2)
+plot(sdf_poly2)
+
+ud3 <- lapply(trialkernel, function(x) try(getverticeshr(x, 50)))
+
+# changing each polygons id to the species name for rbind call
+
+sapply(1:length(ud3), function(i) {
+  row.names(ud3[[i]]) <<- kd_names[i]
+})
+sdf_poly3 <- Reduce(rbind, ud3)
+plot(sdf_poly3)
+
+
+df <- fortify(sdf_poly)
+df2 <- fortify(sdf_poly2)
+df3 <- fortify(sdf_poly3)
+
+g <- ggplot() +
+  geom_polygon(data=df3, aes(x = long, y = lat, fill = id, group = group),alpha = .4) +
+  geom_polygon(data=df2, aes(x = long, y = lat, fill = id, group = group),alpha = .4) +
+  geom_polygon(data=df, aes(x = long, y = lat, fill = id, group = group),alpha = .4) +
+  ggthemes::scale_fill_gdocs() +
+  #coord_equal() +
+  geom_sf(data = world) +
+  coord_sf(xlim = range(for_kernel$lon), ylim = range(for_kernel$lat)) +
+  theme_bw()+
+  xlab("Longitude")+
+  ylab("Latitude")
+g
+g + facet_wrap(~id)
