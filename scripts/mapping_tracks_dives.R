56da9361c3eb0eac418f42b67cb13578e3e5eded
Francis van Oordt <francis.vanoordtlahoz@mail.mcgill.ca>
2025-01-20 23:25:23 -0500

    reproducibility tweaks

--- scripts/mapping_tracks_dives.R
+++ scripts/mapping_tracks_dives.R
@@ -1,4 +1,9 @@
-library(tidyverse)
+library(adehabitatHR)
+library(ggplot2)
+library(dplyr)
+
+##need to update code to SF or terra compatible
+# maybe switch to ctmm
 
 #diving depths and maps
 
@@ -9,40 +14,40 @@ unique(divingdepths$dep_id)
 head(divingdepths)
 
 #this one includes the sitting rock bird C02
-depth_gps_data <-readRDS("C:/Users/francis van oordt/OneDrive - McGill University/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/axxy_depth_peru/data/merged.tdr.gps.RDS")
+depth_gps_data <-readRDS("E:/05BACKUP July 10 2024/Documents/McGill/00Res Prop v2/Chap 1 - DLW axxy/axxy_depth_peru/data/merged.tdr.gps.RDS")
 
-nrow(depth_gps_data %>% 
-  filter(in_dive == 1) )
+nrow(depth_gps_data |> 
+  dplyr::filter(in_dive == 1) )
 
-depth_gps_data %>% 
-  group_by(dep_id) %>% #or per dep ID?
-  filter(in_dive == 1) %>% 
-  summarise(
+depth_gps_data |> 
+  dplyr::group_by(dep_id)|>  #or per dep ID?
+  dplyr::filter(in_dive == 1) |> 
+  dplyr::summarise(
     MaxDepth = max(depth),
     MinDepth = min(depth), 
     meanDepth = mean(depth),
     sdDepth = sd(depth),
     .groups = "drop" 
-  ) %>% 
+  ) |> 
   View()
 
 #need to add the base map! or a terrain basemap
 
-world <- sf::st_read("tracks_and_phys/tracks_and_phys/data/gadm36_PER_shp/gadm36_PER_0.shp")
+world <- sf::st_read("data/gadm36_PER_shp/gadm36_PER_0.shp")
 
-depth_gps_data %>% 
-  filter(in_dive == 1) %>%
-  filter(species == "PEBO") %>% 
-  ggplot()+
-  geom_sf(data = world, aes()) +  ##add basemap
-  coord_sf(crs = 4326, xlim = range(depth_gps_data$lon), ylim = range(depth_gps_data$lat))+# +
-  geom_point(
-    aes(x= lon, y=lat, colour = as.factor(species)), cex = 0.01
+depth_gps_data |>  
+  dplyr::filter(in_dive == 1) |> 
+  dplyr::filter(species == "PEBO") |>  
+  ggplot2::ggplot()+
+  ggplot2::geom_sf(data = world, ggplot2::aes()) +  ##add basemap
+  ggplot2::coord_sf(crs = 4326, xlim = range(depth_gps_data$lon), ylim = range(depth_gps_data$lat))+# +
+  ggplot2::geom_point(
+    ggplot2::aes(x= lon, y=lat, colour = as.factor(species)), cex = 0.01
   )+
-  theme_bw()+
-  xlab("Longitude")+
-  ylab("Latitude")+
-  theme( legend.position = "none")
+  ggplot2::theme_bw()+
+  ggplot2::xlab("Longitude")+
+  ggplot2::ylab("Latitude")+
+  ggplot2::theme( legend.position = "none")
 
 #maybe plot the change in dive locations with a change in day, every two days or so
 
@@ -50,10 +55,10 @@ depth_gps_data %>%
 
 #kernels
 
-for_kernel <-depth_gps_data %>% 
-  filter(in_dive == 1) %>% 
-  mutate(
-    colorKernel = case_when(
+for_kernel <- depth_gps_data |>  
+  dplyr::filter(in_dive == 1) |>  
+  dplyr::mutate(
+    colorKernel = dplyr::case_when(
       species == "PEBO" ~ "cyan",
       species == "GUCO" ~ "brown1"
     )
@@ -61,8 +66,8 @@ for_kernel <-depth_gps_data %>%
 
 for (i in unique(for_kernel$species)) {
   
-  oneSpecies <- for_kernel %>% 
-    filter(species == i)
+  oneSpecies <- for_kernel |>  
+    dplyr::filter(species == i)
   
   onespf <- sf::st_as_sf(oneSpecies, 
                          coords = c("lon","lat"),
@@ -79,25 +84,25 @@ for (i in unique(for_kernel$species)) {
   
   
   
-  p <-ggplot() + 
-    geom_sf(data= sf::st_as_sf(kernelTest50), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
+  p <-ggplot2::ggplot() + 
+    ggplot2::geom_sf(data= sf::st_as_sf(kernelTest50), ggplot2::aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
     )+
-    geom_sf(data= sf::st_as_sf(kernelTest75), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel) #, aes(fill = id, alpha = 0.5)
+    ggplot2::geom_sf(data= sf::st_as_sf(kernelTest75), ggplot2::aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel) #, aes(fill = id, alpha = 0.5)
     )+
-    geom_sf(data= sf::st_as_sf(kernelTest95), aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
+    ggplot2::geom_sf(data= sf::st_as_sf(kernelTest95), ggplot2::aes(alpha = 0.5), fill = unique(oneSpecies$colorKernel)#, aes(fill = id, alpha = 0.5)
     )+
     #geom_sf(data= sf::st_as_sf(one.grizzly.mcp), aes(fill = id, alpha = 0.5)) +
     #scale_fill_discrete(name = "Animal id")+
     #geom_point(data = xy.obs, aes(x=LONGITUDE , y=LATITUDE))+
-    geom_sf(data = world) +
-    coord_sf(crs = 4326, xlim = range(for_kernel$lon), ylim = range(for_kernel$lat)) +
+    ggplot2::geom_sf(data = world) +
+    ggplot2::coord_sf(crs = 4326, xlim = range(for_kernel$lon), ylim = range(for_kernel$lat)) +
     #annotate(geom="text", x=-78.6, y=-8.4, label="2018", size= 13,
     #        color="black") +
-    guides(color = guide_legend(override.aes = list(size = 4)) )+ 
-    guides(alpha = "none", color = "none", size = "none")+
-    ggtitle(i)+
-    theme_bw()+
-    theme(legend.position = "none")
+    ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(size = 4)) )+ 
+    ggplot2::guides(alpha = "none", color = "none", size = "none")+
+    ggplot2::ggtitle(i)+
+    ggplot2::theme_bw()+
+    ggplot2::theme(legend.position = "none")
   print(p)
   
 }
@@ -113,13 +118,16 @@ trialkernel<-adehabitatHR::kernelUD(trialspdf[,8])
 
 image(trialkernel[[2]])
 
-plot(getverticeshr(trialkernel[[1]], 95), add = TRUE)
+## not working without calling package, future fix no important
+#plot(getverticeshr(trialkernel[[1]], 95), add = TRUE)
+
+plot(sf::st_as_sf(adehabitatHR::getverticeshr(trialkernel[[1]], 95), add = TRUE))
 
 # creating SpatialPolygonsDataFrame
 
 kd_names <- names(trialkernel)
 
-ud <- lapply(trialkernel, function(x) try(getverticeshr(x, 95)))
+ud <- lapply(trialkernel, function(x) try(adehabitatHR::getverticeshr(x, 95)))
 
 # changing each polygons id to the species name for rbind call
 
@@ -127,19 +135,23 @@ sapply(1:length(ud), function(i) {
   row.names(ud[[i]]) <<- kd_names[i]
 })
 sdf_poly <- Reduce(rbind, ud)
-plot(sdf_poly)
 
-ud2 <- lapply(trialkernel, function(x) try(getverticeshr(x, 70)))
+terra::plot(terra::vect(sdf_poly))
+
+ud2 <- lapply(trialkernel, function(x) try(adehabitatHR::getverticeshr(x, 70)))
 
 # changing each polygons id to the species name for rbind call
 
 sapply(1:length(ud2), function(i) {
   row.names(ud2[[i]]) <<- kd_names[i]
 })
-sdf_poly2 <- Reduce(rbind, ud2)
-plot(sdf_poly2)
 
-ud3 <- lapply(trialkernel, function(x) try(getverticeshr(x, 50)))
+sdf_poly2 <- Reduce(rbind, ud2)
+
+#not working well without the call of the library
+plot(sf::st_as_sf(sdf_poly2))
+
+ud3 <- lapply(trialkernel, function(x) try(adehabitatHR::getverticeshr(x, 50)))
 
 # changing each polygons id to the species name for rbind call
 
@@ -147,12 +159,20 @@ sapply(1:length(ud3), function(i) {
   row.names(ud3[[i]]) <<- kd_names[i]
 })
 sdf_poly3 <- Reduce(rbind, ud3)
-plot(sdf_poly3)
 
+#not working well without the call of the library
+
+plot(sf::st_as_sf(sdf_poly3))
+
+
+library(adehabitatHR)
+library(ggplot2)
 
 df <- fortify(sdf_poly)
 df2 <- fortify(sdf_poly2)
 df3 <- fortify(sdf_poly3)
+
+locIsland<-data.frame(y = -8.534234, x = -78.962968)
 
 g <- ggplot() +
   geom_polygon(data=df3, aes(x = long, y = lat, fill = id, group = group),alpha = .4) +
@@ -161,9 +181,15 @@ g <- ggplot() +
   ggthemes::scale_fill_gdocs() +
   #coord_equal() +
   geom_sf(data = world) +
+  ggplot2::geom_point(data =locIsland, ggplot2::aes(x = x, y = y), pch = 2, cex= 2)+
   coord_sf(xlim = range(for_kernel$lon), ylim = range(for_kernel$lat)) +
   theme_bw()+
+  ggplot2::labs(fill = "Species")+
   xlab("Longitude")+
   ylab("Latitude")
 g
+
+ggplot2::ggsave(g, filename = "plots/kernel_dives_2019.png", bg="white", units = "in",
+                height =4 ,
+               width = 5, dpi = 300)
 g + facet_wrap(~id)
